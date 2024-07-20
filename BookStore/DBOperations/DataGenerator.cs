a1f2c53e16086a16c2685f3bf8e4fd013d2c945a
Arda Coşkun <arda.coskun@erkalabs.com>
2024-07-20 13:49:05 +0300

    auto incremented id update

--- BookStore/DBOperations/DataGenerator.cs
+++ BookStore/DBOperations/DataGenerator.cs
@@ -16,9 +16,9 @@ public class DataGenerator
             }
 
             context.Books.AddRange(
-                new Book { Id = 1, Title = "Lean Startup", GenreId = 1, PageCount = 200, PublishDate = new DateTime(2001, 06, 12) },
-        new Book { Id = 2, Title = "Herland", GenreId = 2, PageCount = 250, PublishDate = new DateTime(2010, 06, 12) },
-        new Book { Id = 3, Title = "Dune2", GenreId = 2, PageCount = 540, PublishDate = new DateTime(2001, 12, 21) }
+                new Book { Title = "Lean Startup", GenreId = 1, PageCount = 200, PublishDate = new DateTime(2001, 06, 12) },
+        new Book { Title = "Herland", GenreId = 2, PageCount = 250, PublishDate = new DateTime(2010, 06, 12) },
+        new Book { Title = "Dune2", GenreId = 2, PageCount = 540, PublishDate = new DateTime(2001, 12, 21) }
             );
 
             context.SaveChanges();
